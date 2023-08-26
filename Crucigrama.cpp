3290ac68cca34702c38b4870f05cc2ca746b2381
Sebastián Sánchez <109748087+S6bastian@users.noreply.github.com>
2023-08-26 11:22:35 -0500

    Falta

---
+++ Crucigrama.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    //char contenido[1][6] = { {A,J,P,A,T,O}, };
+    char** crucigrama = new char* [5];
+    for (char** fila = crucigrama; fila < crucigrama + 5; fila++){
+        for (char* columna = *fila; columna < *fila + 6; columna++) {
+
+        }
+    }
+    
+
+
+}
