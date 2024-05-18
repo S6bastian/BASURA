bdca33ea34f1913c74803fdbcecb985eb6f648e7
Sebastián Sánchez <109748087+S6bastian@users.noreply.github.com>
2024-05-18 11:15:48 -0500

    Update P1.cpp

--- P1.cpp
+++ P1.cpp
@@ -1,19 +1,109 @@
-// P1.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
+// CicrcularLinkedList.cpp : Este archivo contiene la función "main". La ejecución del programa comienza y termina ahí.
 //
 
 #include <iostream>
 
+using namespace std;
+
+class node
+{
+public:
+    int data;
+    node* next;
+};
+
+class CircularLinkedList
+{
+    node* head = NULL;
+public:
+    bool find(int value, node**& current) {
+        current = &head;
+        if (head == nullptr)
+            return 0;
+
+
+
+        bool switcher = 0;
+        current = &(*current)->next;
+        while (switcher == 0) {
+            current = &(*current)->next;
+            if (value < head->data) {
+                if (*current == head) {
+                    switcher++;
+                    continue;
+                }
+            }
+
+            if (value < (*current)->data)
+                break;
+            if (*current == head)
+                switcher++;
+        }
+        if ((*current)->data == value)
+            return 1;
+        
+        return 0;
+
+    }
+
+    void add(int value)
+    {
+        //TO DO
+
+        node** current;
+
+        if (!find(value, current)) {
+            node* tmp = *current;
+            *current = new node();
+            (*current)->data = value;
+            (*current)->next = tmp;
+            return;
+        }
+
+        
+        
+
+    }
+
+    void del(int value)
+    {
+        // TO DO
+
+    }
+
+    void print()
+    {
+        int cont = 0;
+        node* ptr = head;
+        cout << "head->";
+        while (head && cont < 1)
+        {
+            cout << ptr->data << " -> ";
+            ptr = ptr->next;
+            if (ptr == head) cont++;
+        }
+        if (head) cout << ptr->data;
+        cout << " <- head \n ";
+
+    }
+};
+
 int main()
 {
-    std::cout << "Hello World!\n";
+    int ADD[10] = { 2,4,6,8,10,1,3,5,7,9 };
+    int DEL[10] = { 9,7,5,3,1,10,8,6,4,2 };
+    CircularLinkedList CLL;
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "ADD " << ADD[i] << endl;
+        CLL.add(ADD[i]);
+        CLL.print();
+    }
+
+    for (int i = 0; i < 10; i++)
+    {
+        cout << "DEL " << DEL[i] << endl;
+        CLL.del(DEL[i]);
+        CLL.print();
+    }
 }
-
-// Ejecutar programa: Ctrl + F5 o menú Depurar > Iniciar sin depurar
-// Depurar programa: F5 o menú Depurar > Iniciar depuración
-
-// Sugerencias para primeros pasos: 1. Use la ventana del Explorador de soluciones para agregar y administrar archivos
-//   2. Use la ventana de Team Explorer para conectar con el control de código fuente
-//   3. Use la ventana de salida para ver la salida de compilación y otros mensajes
-//   4. Use la ventana Lista de errores para ver los errores
-//   5. Vaya a Proyecto > Agregar nuevo elemento para crear nuevos archivos de código, o a Proyecto > Agregar elemento existente para agregar archivos de código existentes al proyecto
-//   6. En el futuro, para volver a abrir este proyecto, vaya a Archivo > Abrir > Proyecto y seleccione el archivo .sln
