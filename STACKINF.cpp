0ed1006c65564481e771d42c93b9734bfe8ee96c
Sebastián Sánchez <109748087+S6bastian@users.noreply.github.com>
2025-04-05 11:27:37 -0500

    Add files via upload

---
+++ STACKINF.cpp
@@ -0,0 +1,106 @@
+#include <iostream>
+using namespace std;
+
+template<class T>
+struct Node{
+	T array[5];
+	int size;
+	Node* next;
+	
+	Node(PNext = nullptr){
+		next = PNext;
+		size = 5;
+	}
+};
+
+struct StackInf{
+	T* top;
+	Node* currentNode;
+	
+	StackInf(){
+		next = nullptr;
+		currentNode = nullptr;
+	}
+		
+	void push(T v){
+		if(top == nullptr){
+			currentNode = new Node;
+			top = currentNode->array;
+			*top = v;
+			return;
+		}
+		
+		if(top > currentNode->size-1){
+			currentNode = new Node(currentNode);
+			top = currentNode->array;
+			*top = v;
+			return;
+		}
+		
+		*(++top) = v;
+	}
+	
+	void pop(T& v){
+		if(top == nullptr){
+			v = -1;
+			return;
+		}
+		if(top == currentNode->array){
+			v = *top;
+			if(currentNode->next == nullptr){
+				delete[] currentNode;
+				currentNode = nullptr;
+				top = nullptr;
+			}
+			else{
+				Node* tmp = currentNode;
+				currentNode = currentNode->next;
+				delete[] tmp;
+				top = currentNode->array + currentNode->size - 1;
+			}
+			return;
+		}
+		v = top;
+		top--;
+	}
+		
+	void print(){
+		T* pTop = top;
+		Node* pNode = currentNode;
+		
+		cout << "TOP [";
+		while(true){
+			cout << " " << pTop;
+			if(pTop == pNode->array){
+				if(pNode->next == nullptr){
+					cout << " ] BOTTOM" << endl;
+					break;
+				}
+				pNode = pNode->next;
+				pTop = pNode->array + pNode->size - 1;
+				cout << " ] -----> ["
+			}
+			else pTop--;
+		}
+	}
+}
+
+int main() {
+	StackInf<int> S;
+	
+	for(int i = 0; i < 12; i++){
+		S.push(i);
+	}
+	S.print();
+	for(int i = 0; i < 3; i++){
+		int v;
+		S.pop(v);
+		cout << "POPPED: " << v;
+		cout << endl;
+	}
+	S.print();
+	
+	
+	return 0;
+}
+
---
+++ compare.cpp
@@ -0,0 +1,192 @@
+#include <iostream>
+#include <algorithm>
+#include <vector>
+#include <chrono>
+
+using namespace std;
+using namespace chrono;
+
+// ---------------------------------------------------------------------
+// Polymorphism
+// ---------------------------------------------------------------------
+
+template<class T>
+struct PCompare {
+	virtual bool cmp(T a, T b) = 0;
+	virtual ~PCompare() {};
+};
+
+template<class T>
+struct PGreater : PCompare<T> {
+	bool cmp(T a, T b) { return a > b; }
+};
+
+template<class T>
+struct PLesser : PCompare<T> {
+	bool cmp(T a, T b) { return a < b; }
+};
+
+// ---------------------------------------------------------------------
+// Function Object
+// ---------------------------------------------------------------------
+
+template<class T>
+struct FOGreater {
+	bool operator()(T a, T b) { return a > b; }
+};
+
+template<class T>
+struct FOLesser {
+	bool operator()(T a, T b) { return a < b; }
+};
+
+// ---------------------------------------------------------------------
+// Function Pointer
+// ---------------------------------------------------------------------
+
+template<class T>
+bool Gcmp(T a, T b) { return a > b; }
+
+template<class T>
+	bool Lcmp(T a, T b) { return a < b; }
+	
+	template<class T>
+		bool (*FPGreater)(T a, T b) = Gcmp;
+		
+		template<class T>
+		bool (*FPLesser)(T a, T b) = Lcmp;
+		
+		// ---------------------------------------------------------------------
+		
+		template<class T, class O>
+		struct PAJAROTE {	//BubbleSort Methods
+			O C;
+			
+			void print(vector<T>& array, int n) {
+				for (int i = 0; i < n; i++) {
+					cout << array[i] << " ";
+				}
+				cout << endl;
+			}
+			
+			// Direct TV
+			void bubbleSort(vector<int> array, int n) {
+				auto start = high_resolution_clock::now();
+				
+				for (int i = 0; i < n; i++) {
+					for (int j = 1; j < n; j++) {
+						if (array[j - 1] < array[j]) swap(array[j - 1], array[j]);
+					}
+				}
+				
+				auto end = high_resolution_clock::now();
+				duration<double> duration_ms = end - start;
+				
+				//print(array, n);
+				
+				cout << "Direct TV Time: " << duration_ms.count() << " ms" << endl;
+			}
+			
+			// Polymorphism
+			void PbubbleSort(vector<int> array, int n) {
+				PCompare<T>* C = new PLesser<T>;
+				
+				auto start = high_resolution_clock::now();
+				
+				/*
+				PGreater<T> G;
+				C = &G;
+				*/
+				
+				for (int i = 0; i < n; i++) {
+					for (int j = 1; j < n; j++) {
+						if (C->cmp(array[j - 1], array[j])) swap(array[j - 1], array[j]);
+					}
+				}
+				
+				auto end = high_resolution_clock::now();
+				duration<double> duration_ms = end - start;
+				
+				//print(array, n);
+				delete C;
+				
+				cout << "Polymorphism Time: " << duration_ms.count() << " ms" << endl;
+			}
+			
+			// Function Object
+			void FObubbleSort(vector<int> array, int n) {
+				
+				
+				auto start = high_resolution_clock::now();
+				
+				for (int i = 0; i < n; i++) {
+					for (int j = 1; j < n; j++) {
+						if (C(array[j - 1], array[j])) swap(array[j - 1], array[j]);
+					}
+				}
+				
+				auto end = high_resolution_clock::now();
+				duration<double> duration_ms = end - start;
+				
+				//print(array, n);
+				
+				cout << "Function Object Time: " << duration_ms.count() << " ms" << endl;
+			}
+			
+			// Function Pointer
+			void FPbubbleSort(vector<int> array, int n) {
+				auto start = high_resolution_clock::now();
+				
+				for (int i = 0; i < n; i++) {
+					for (int j = 1; j < n; j++) {
+						if (FPLesser<T>(array[j - 1], array[j])) swap(array[j - 1], array[j]);
+					}
+				}
+				
+				auto end = high_resolution_clock::now();
+				duration<double> duration_ms = end - start;
+				
+				//print(array, n);
+				
+				cout << "Function Pointer Time: " << duration_ms.count() << " ms" << endl;
+			}
+		};
+		
+		template<class T, class O>
+		void solve(vector<int> array, int n) {
+			
+			PAJAROTE<T, O> Methods;
+			
+			cout << "Polymorphism" << endl;
+			Methods.PbubbleSort(array, n);
+			cout << endl << endl;
+			
+			cout << "Function Pointer" << endl;
+			Methods.FPbubbleSort(array, n);
+			cout << endl << endl;
+			
+			cout << "Function Object" << endl;
+			Methods.FObubbleSort(array, n);
+			cout << endl << endl;
+			
+			cout << "Direct TV" << endl;
+			Methods.bubbleSort(array, n);
+			cout << endl << endl;
+		}
+		
+		int main() {
+			int n = 1000000;
+			//cin >> n;
+			
+			vector<int> array(n);
+			for (int i = 0; i < n; i++) {
+				array[i] = i;
+			}
+			
+			while (true) {
+				cin >> n;
+				solve<int, FOLesser <int> >(array, n);
+			}
+			return 0;
+		}
+		
