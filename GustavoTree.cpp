e9c3cda3b43d622f392aa0751083d809b49659ff
Sebastián Sánchez <109748087+S6bastian@users.noreply.github.com>
2025-06-14 11:10:40 -0500

    Create GustavoTree.cpp

---
+++ GustavoTree.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <vector>
+#include <queue>
+using namespace std;
+
+template<class T>
+struct Node{
+	T value;
+	Node<T>* next[2];
+	
+	Node(T v){
+		value = v;
+		next[0] = next[1] = nullptr;
+	}
+};
+
+struct GustavoTree{
+	root = nullptr;
+	
+	GustavoTree(){
+		
+	}
+	
+	
+};
+
+int main() {
+	
+	
+	
+	return 0;
+}
+
